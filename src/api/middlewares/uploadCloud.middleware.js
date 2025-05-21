d1cd364d2ab3d18be3654bce60c6a2d4f411abff
Thành Zanh <114275350+thanhzanh@users.noreply.github.com>
2025-05-21 23:09:29 +0700

    Chuc nang upload anh len cloudinary online

---
+++ src/api/middlewares/uploadCloud.middleware.js
@@ -0,0 +1,37 @@
+const cloudinary = require('cloudinary').v2
+const streamifier = require('streamifier');
+require('dotenv').config();
+
+// Configuration
+cloudinary.config({ 
+    cloud_name: process.env.CLOUD_NAME, 
+    api_key: process.env.CLOUD_KEY, 
+    api_secret: process.env.CLOUD_SECRET // Click 'View API Keys' above to copy your API secret
+});
+
+module.exports.upload = async (req, res, next) => {
+    try {
+        let streamUpload = (req) => {
+            return new Promise((resolve, reject) => {
+                let stream = cloudinary.uploader.upload_stream(
+                    (error, result) => {
+                    if (result) {
+                        resolve(result);
+                    } else {
+                        reject(error);
+                    }
+                    }
+                );
+
+                streamifier.createReadStream(req.file.buffer).pipe(stream);
+            });
+        };
+
+        let result = await streamUpload(req);
+        console.log(result.secure_url);
+        req.body.url_hinh_dai_dien = result.secure_url;
+    } catch (error) {
+        res.status(500).json({ message: "Upload ảnh thất bại", error });
+    }
+    next();
+};
--- src/api/routes/user.route.js
+++ src/api/routes/user.route.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const router = express.Router();
+const multer = require('multer');
+const upload = multer();
 
 const controller = require('../controllers/user.controller');
 const { authenticateUser, authorizeRole } = require('../middlewares/auth.middleware');
 const validate = require('../middlewares/validate.middleware');
+
+const uploadCloud = require('../middlewares/uploadCloud.middleware');
 
 // Lấy danh sách tất cả người dùng tìm kiếm, phân trang (quản trị viên)
 router.get("/", authenticateUser, authorizeRole(["quan_tri_vien"]), controller.index);
@@ -21,7 +25,14 @@ router.put("/edit/:id_nguoi_dung", authenticateUser, authorizeRole(["quan_tri_vi
 router.put("/change-status/:id_nguoi_dung", authenticateUser, authorizeRole(["quan_tri_vien"]), controller.changeStatus);
 
 // Cập nhật thông tin cá nhân người dùng 
-router.put("/update-profile", authenticateUser, authorizeRole(["nguoi_dung"]), validate.updateProfileValidation, controller.updateProfile);
+router.put("/update-profile",
+    authenticateUser,
+    authorizeRole(["nguoi_dung"]),
+    upload.single("url_hinh_dai_dien"),
+    uploadCloud.upload,
+    validate.updateProfileValidation,
+    controller.updateProfile
+);
 
 // Lấy thông tin một người dùng
 router.get("/profile", authenticateUser, authorizeRole(["nguoi_dung"]), controller.getProfile);
